8dd1979f426bbad217506e99114b831bc9b471d4
maycoll611 <61752048+maycoll611@users.noreply.github.com>
2021-03-09 16:25:59 -0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/';
+$config['base_url'] = 'https://github.com/maycoll611/ecoman/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'ecoman';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
